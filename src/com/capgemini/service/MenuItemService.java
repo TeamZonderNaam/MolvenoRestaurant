c2c10c0ac3041da358113535d6db643f3369a0f0
Robin de Jong <robin.jong@capgemini.com>
2018-09-17 14:40:27 +0200

    Added an example model and service

---
+++ src/com/capgemini/service/MenuItemService.java
@@ -0,0 +1,10 @@
+package com.capgemini.service;
+
+import com.capgemini.model.MenuItem;
+
+public class MenuItemService implements Service {
+    @Override
+    public boolean add(MenuItem item) {
+        return false;
+    }
+}
---
+++ src/com/capgemini/service/Service.java
@@ -0,0 +1,7 @@
+package com.capgemini.service;
+
+import com.capgemini.model.MenuItem;
+
+public interface Service {
+    boolean add(MenuItem item);
+}
