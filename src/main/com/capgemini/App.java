de8501f83e1c417a81494c22030af4c02b036123
tamim <t.djouya@live.nl>
2018-09-20 13:44:03 +0200

    Update App.java

--- src/main/com/capgemini/App.java
+++ src/main/com/capgemini/App.java
@@ -1,9 +1,5 @@
 package com.capgemini;
 
-import com.capgemini.Controller.MenuController;
-import com.capgemini.Controller.MenuItemController;
-import com.capgemini.service.MenuItemService;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("Hallo");
